a4176fa41ce8ed071daa40cf81d6c36ba5795b8c
Chris <elysuim_1@hotmail.com>
2021-04-28 22:25:00 +0100

    Update HelperFunctions.py

--- Backend/HelperFunctions.py
+++ Backend/HelperFunctions.py
@@ -6,6 +6,9 @@ import matplotlib.pyplot as plt
 
 from Backend.SessionUser import SessionUser
 
+MAX_QUESTIONS_PER_API_CALL = 50
+GENERAL_KNOWLEDGE = 9
+
 def generate_probabilities(awnser_count:int, num_options:int) -> List[float]:
     return [round(i, 2) for i in linspace(1/awnser_count, 1, num_options)]
     
@@ -15,7 +18,6 @@ def get_questions(number_of_questions:int) -> Optional[List[Dict[str, Any]]]:
     Fetch quesions by calling API at https://opentdb.com/
     """
 
-    GENERAL_KNOWLEDGE = 9
     url = f"https://opentdb.com/api.php?amount={number_of_questions}&category={GENERAL_KNOWLEDGE}&encode=url3986"
     response = requests.get(url)
     
