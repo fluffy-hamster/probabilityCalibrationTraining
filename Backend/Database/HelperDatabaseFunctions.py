a05f8e6762411ef55883512827123d8f6f9c2102
Chris <elysuim_1@hotmail.com>
2021-04-28 10:46:29 +0100

    Update HelperDatabaseFunctions.py

--- Backend/Database/HelperDatabaseFunctions.py
+++ Backend/Database/HelperDatabaseFunctions.py
@@ -21,5 +21,19 @@ CREATE TABLE Session
 )
 '''
 
+
 def connect_to_database(db_path: str):
     return sqlite3.connect(db_path)
+
+
+def addUserCommand(username: str) -> str:
+    return f"""
+    INSERT INTO User (username)
+    VALUES ({username})
+    """
+
+def addSessionCommand(session_id: int, user_id: int, interval: float, correct_answers: int, wrong_answers: int) -> str:
+    return f"""
+    INSERT INTO Session (sessionId, userId, interval, correctAnswers, wrongAnswers)
+    VALUES ({session_id}, {user_id}, {interval}, {correct_answers}, {wrong_answers})
+    """
