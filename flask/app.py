e23af2b03f3c1029e155126b864c93f20ef4573a
Chris <elysuim_1@hotmail.com>
2021-05-08 19:50:18 +0100

    Update app.py

--- flask/app.py
+++ flask/app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, request
+from flask.globals import session
+
 app = Flask(__name__)
 
 @app.route('/')
 def index():
   return 'Server Works!'
   
-@app.route('/greet')
+@app.route('/userData/',  methods=['GET'])
 def say_hello():
-  return 'Hello from Server'
+  user_id = request.args.get("userId")
+  session_id = request.args.get("sessionId") 
+  
+  return f'''<h1>Request Args| user_id: {user_id} | session_id: {session_id}</h1>'''
+
+
+
+@app.route('/query-example')
+def query_example():
+    # if key doesn't exist, returns None
+    language = request.args.get('language')
+
+    return '''<h1>The language value is: {}</h1>'''.format(language)
+
+    http://127.0.0.1:5000/query-example?language=Python
